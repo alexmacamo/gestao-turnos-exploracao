4b2ce8a41a4160113fc05cd858ab6a55f835be97
alexmacamo <45073629+alexmacamo@users.noreply.github.com>
2026-05-19 01:07:10 +0200

    Create main.jsx
    Adicionar main React

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PrototipoRelatoriosGestaoTurnos from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PrototipoRelatoriosGestaoTurnos />
+  </React.StrictMode>
+);
